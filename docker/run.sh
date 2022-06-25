ad5c88cb24637c5d06269d570f60ce136ddd2ae7
sakoda <sakoda@keio.jp>
2022-06-25 13:57:03 +0900

    Docker系更新

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,8 @@
-# docker build -t rl_practice:20220619 .
+# docker build -t rl_practice:latest .
 docker run -it \
     --gpus all \
     --ipc=host \
     # --env="DISPLAY" \
     # --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --name rl_practice_20220619 \
-    rl_practice:20220619 bash
+    --name rl_practice_container \
+    rl_practice:latest bash
