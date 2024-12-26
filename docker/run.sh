54710cd24b4dbc24c4ab03d502af267e74fde26a
Shintaro Sakoda <sakoda@keio.jp>
2024-12-26 19:02:27 +0900

    Updated Dockerfile
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,9 @@
-# docker build -t rl_practice:latest .
+docker build -t rl_practice:latest .
 docker run -it \
     --gpus all \
     --ipc=host \
-    # --env="DISPLAY" \
-    # --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="DISPLAY" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume=$HOME/work/rl_practice:/root/work/rl_practice \
     --name rl_practice_container \
     rl_practice:latest bash
