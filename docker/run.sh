188cf014b6eb33d46d7c381f29ed04880c97d007
Shintaro Sakoda <sakoda@keio.jp>
2024-12-26 19:11:07 +0900

    Updated dockerfile
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,17 @@
-docker build -t rl_practice:latest .
+#!/bin/bash
+set -eux
+
+docker build \
+    --build-arg USER_NAME=$(whoami) \
+    --build-arg USER_UID=$(id -u) \
+    --build-arg USER_GID=$(id -g) \
+    -t rl_practice:latest .
+
 docker run -it \
     --gpus all \
     --ipc=host \
     --env="DISPLAY" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume=$HOME/work/rl_practice:/root/work/rl_practice \
+    --volume=$HOME/work/rl_practice:$HOME/work/rl_practice \
     --name rl_practice_container \
     rl_practice:latest bash
