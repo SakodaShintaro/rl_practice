ef39e7d695cfe19b6c66edc04f599a32bb0e724d
Shintaro Sakoda <sakoda@keio.jp>
2025-01-05 13:14:40 +0000

    Added y_key
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- plot.py
+++ plot.py
@@ -15,16 +15,18 @@ if __name__ == "__main__":
     args = parse_args()
     target_dir_list = args.target_dir_list
 
+    y_key = "return"
+
     for target_dir in target_dir_list:
         tsv_path = target_dir / "result.tsv"
         df = pd.read_csv(tsv_path, delimiter="\t")
 
-        sum_steps = df["steps"].cumsum()
+        y_value = df[y_key].cumsum()
 
-        plt.plot(sum_steps, df["steps"], label=str(target_dir.name))
+        plt.plot(y_value, df[y_key], label=str(target_dir.name))
 
     plt.xlabel("Episode")
-    plt.ylabel("steps")
+    plt.ylabel(y_key)
     plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
     plt.grid()
     plt.savefig("result.png", bbox_inches="tight", pad_inches=0.05)
