4ead3776c078055ba24d3fd00111a496c50c0a45
Shintaro Sakoda <sakoda@keio.jp>
2025-01-25 05:06:39 +0000

    Fixed
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def check_sample(env_name: str) -> None:
         print(f"{i=:08d}\t{reward=}", end="\r")
 
         r = env.render()  # (400, 600, 3)
+        r = cv2.cvtColor(r, cv2.COLOR_RGB2BGR)
         cv2.imwrite(str(save_dir / f"{i:08d}.png"), r)
 
         # If the episode has ended then we can reset to start a new episode
@@ -52,11 +53,18 @@ if __name__ == "__main__":
     args = parse_args()
 
     env_name_list = [
+        # Classic control
         "Acrobot-v1",
         "CartPole-v1",
         "MountainCarContinuous-v0",
         "MountainCar-v0",
         "Pendulum-v1",
+        # Box2D
+        "BipedalWalker-v3",
+        "CarRacing-v3",
+        "LunarLander-v3",
+        # MuJoCo
+        "Ant-v5",
     ]
 
     for env_name in env_name_list:
