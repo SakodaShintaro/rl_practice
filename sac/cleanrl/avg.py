558ab0ceee008383657b7be83d0e10211435340d
Shintaro Sakoda <sakoda@keio.jp>
2025-01-30 20:43:13 +0000

    Extract network
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- sac/cleanrl/avg.py
+++ sac/cleanrl/avg.py
@@ -17,115 +17,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-import torch.nn.functional as F
 from gymnasium.wrappers import NormalizeObservation
-from torch import nn
-from torch.distributions import MultivariateNormal
 
 import wandb
-
-
-def orthogonal_weight_init(m: nn.Module) -> None:
-    """Orthogonal weight initialization for neural networks."""
-    if isinstance(m, nn.Linear):
-        nn.init.orthogonal_(m.weight.data)
-        m.bias.data.fill_(0.0)
-
-
-class Actor(nn.Module):
-    """Continous MLP Actor for Soft Actor-Critic."""
-
-    def __init__(self, obs_dim: int, action_dim: int, device: torch.device, n_hid: int) -> None:
-        super().__init__()
-        self.device = device
-        self.LOG_STD_MAX = 2
-        self.LOG_STD_MIN = -5
-
-        # Two hidden layers
-        self.phi = nn.Sequential(
-            nn.Linear(obs_dim, n_hid),
-            nn.LeakyReLU(),
-            nn.Linear(n_hid, n_hid),
-            nn.LeakyReLU(),
-        )
-
-        self.mu = nn.Linear(n_hid, action_dim)
-        self.log_std = nn.Linear(n_hid, action_dim)
-
-        self.apply(orthogonal_weight_init)
-        self.to(device=device)
-
-    def forward(self, obs: torch.Tensor) -> torch.Tensor:
-        """Forward pass through the Actor network."""
-        phi = self.phi(obs.to(self.device))
-        phi = phi / torch.norm(phi, dim=1).view((-1, 1))
-        mu = self.mu(phi)
-        log_std = self.log_std(phi)
-        log_std = torch.clamp(log_std, self.LOG_STD_MIN, self.LOG_STD_MAX)
-
-        dist = MultivariateNormal(mu, torch.diag_embed(log_std.exp()))
-        action_pre = dist.rsample()
-        lprob = dist.log_prob(action_pre)
-        lprob -= (2 * (np.log(2) - action_pre - F.softplus(-2 * action_pre))).sum(axis=1)
-
-        # N.B: Tanh must be applied _only_ after lprob estimation of dist sampled action!!
-        #   A mistake here can break learning :/
-        action = torch.tanh(action_pre)
-        action_info = {
-            "mu": mu,
-            "log_std": log_std,
-            "dist": dist,
-            "lprob": lprob,
-            "action_pre": action_pre,
-        }
-
-        return action, action_info
-
-
-class Q(nn.Module):
-    """Continuous Q-network for Soft Actor-Critic."""
-
-    def __init__(self, obs_dim: int, action_dim: int, device: torch.device, n_hid: int) -> None:
-        super().__init__()
-        self.device = device
-
-        # Two hidden layers
-        self.phi = nn.Sequential(
-            nn.Linear(obs_dim + action_dim, n_hid),
-            nn.LeakyReLU(),
-            nn.Linear(n_hid, n_hid),
-            nn.LeakyReLU(),
-        )
-        self.q = nn.Linear(n_hid, 1)
-        self.apply(orthogonal_weight_init)
-        self.to(device=device)
-
-    def forward(self, obs: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
-        """Forward pass through the Q network."""
-        x = torch.cat((obs, action), -1).to(self.device)
-        phi = self.phi(x)
-        phi = phi / torch.norm(phi, dim=1).view((-1, 1))
-        return self.q(phi).view(-1)
+from network import Actor, SoftQNetwork
 
 
 class AVG:
     """AVG Agent."""
 
-    def __init__(self, cfg: argparse.Namespace) -> None:
+    def __init__(self, cfg: argparse.Namespace, env: gym.Env) -> None:
         self.steps = 0
 
-        self.actor = Actor(
-            obs_dim=cfg.obs_dim,
-            action_dim=cfg.action_dim,
-            device=cfg.device,
-            n_hid=cfg.nhid_actor,
-        )
-        self.Q = Q(
-            obs_dim=cfg.obs_dim,
-            action_dim=cfg.action_dim,
-            device=cfg.device,
-            n_hid=cfg.nhid_critic,
-        )
+        self.actor = Actor(env)
+        self.Q = SoftQNetwork(env)
 
         self.actor_lr = cfg.actor_lr
         self.critic_lr = cfg.critic_lr
@@ -156,8 +61,8 @@ class AVG:
     def compute_action(self, obs: np.ndarray) -> tuple[torch.Tensor, dict]:
         """Compute the action and action information given an observation."""
         obs = torch.Tensor(obs.astype(np.float32)).unsqueeze(0).to(self.device)
-        action, action_info = self.actor(obs)
-        return action, action_info
+        action, log_prob, mean = self.actor.get_action(obs)
+        return action, log_prob, mean
 
     def update(
         self,
@@ -166,18 +71,17 @@ class AVG:
         next_obs: np.ndarray,
         reward: float,
         done: bool,
-        **kwargs: dict,
+        lprob: torch.Tensor,
     ) -> None:
         """Update the actor and critic networks based on the observed transition."""
         obs = torch.Tensor(obs.astype(np.float32)).unsqueeze(0).to(self.device)
         next_obs = torch.Tensor(next_obs.astype(np.float32)).unsqueeze(0).to(self.device)
-        action, lprob = action.to(self.device), kwargs["lprob"]
+        action, lprob = action.to(self.device), lprob.to(self.device)
 
         #### Q loss
         q = self.Q(obs, action.detach())  # N.B: Gradient should NOT pass through action here
         with torch.no_grad():
-            next_action, action_info = self.actor(next_obs)
-            next_lprob = action_info["lprob"]
+            next_action, next_lprob, mean = self.actor.get_action(next_obs)
             q2 = self.Q(next_obs, next_action)
             target_V = q2 - self.alpha_lr * next_lprob
 
@@ -347,7 +251,7 @@ if __name__ == "__main__":
     args.obs_dim = env.observation_space.shape[0]
     args.action_dim = env.action_space.shape[0]
 
-    agent = AVG(args)
+    agent = AVG(args, env)
 
     action_coeff = (env.action_space.high - env.action_space.low) / 2
 
@@ -369,7 +273,7 @@ if __name__ == "__main__":
 
     for total_step in range(1, args.N + 1):
         # N.B: Action is a torch.Tensor
-        action, action_info = agent.compute_action(obs)
+        action, lprob, mean = agent.compute_action(obs)
         sim_action = action.detach().cpu().view(-1).numpy()
         sim_action *= action_coeff
 
@@ -381,9 +285,9 @@ if __name__ == "__main__":
             save_image_dir.mkdir(exist_ok=True, parents=True)
             image = env.render()
             cv2.imwrite(str(save_image_dir / f"{ep_step:08d}.png"), image)
-        stats = agent.update(obs, action, next_obs, reward_normed, terminated, **action_info)
+        stats = agent.update(obs, action, next_obs, reward_normed, terminated, lprob)
         sum_delta += stats["delta"]
-        sum_lprob += action_info["lprob"].item()
+        sum_lprob += lprob.item()
         sum_reward += reward
         sum_reward_normed += reward_normed
         ep_step += 1
