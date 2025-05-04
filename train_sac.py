d51c631a17e105f0e1930d5b33e5a53447d1b678
sakoda <sakoda@keio.jp>
2025-05-04 12:07:57 +0900

    Fixed to reuse log_pi
    Signed-off-by: sakoda <sakoda@keio.jp>

--- train_sac.py
+++ train_sac.py
@@ -177,9 +177,7 @@ if __name__ == "__main__":
         actor_loss.backward()
         actor_optimizer.step()
 
-        with torch.no_grad():
-            _, log_pi, _ = actor.get_action(encoder(data.observations))
-        alpha_loss = (-log_alpha.exp() * (log_pi + target_entropy)).mean()
+        alpha_loss = (-log_alpha.exp() * (log_pi.detach() + target_entropy)).mean()
 
         a_optimizer.zero_grad()
         alpha_loss.backward()
