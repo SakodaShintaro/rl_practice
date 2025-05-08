ccde251a862307f0fd09f2db27bb22ec0a4ec87a
sakoda <sakoda@keio.jp>
2025-05-08 22:37:56 +0900

    Added backbones
    Signed-off-by: sakoda <sakoda@keio.jp>

--- networks/backbone.py
+++ networks/backbone.py
@@ -1,3 +1,4 @@
+from diffusers.models import AutoencoderKL, AutoencoderTiny
 from torch import nn
 
 
@@ -23,3 +24,48 @@ class BaseCNN(nn.Module):
 
     def forward(self, x):
         return self.features(x)
+
+
+class AE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ae = AutoencoderTiny.from_pretrained("madebyollin/taesd", cache_dir="./pretrained")
+
+    def encode(self, x):
+        return self.ae.encode(x).latents
+
+    def decode(self, x):
+        return self.ae.decode(x).sample
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vae = AutoencoderKL.from_pretrained(
+            "stabilityai/sd-vae-ft-ema", cache_dir="./pretrained"
+        )
+        self.scale = 0.18215
+
+    def encode(self, x):
+        return self.vae.encode(x).latent_dist.sample().mul_(self.scale)
+
+    def decode(self, x):
+        return self.vae.decode(x / self.scale).sample
+
+
+if __name__ == "__main__":
+    import torch
+
+    model_ae = AE()
+    x = torch.randn(1, 3, 96, 96)
+    output_enc = model_ae.encode(x)
+    print(output_enc.shape)  # (1, 4, 12, 12)
+    output_dec = model_ae.decode(output_enc)
+    print(output_dec.shape)  # (1, 3, 96, 96)
+
+    model_vae = VAE()
+    x = torch.randn(1, 3, 96, 96)
+    output_enc = model_vae.encode(x)
+    print(output_enc.shape)  # (1, 4, 12, 12)
+    output_dec = model_vae.decode(output_enc)
+    print(output_dec.shape)  # (1, 3, 96, 96)
