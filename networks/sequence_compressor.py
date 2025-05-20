3c7d394f29039963b039e275d2a0de1e0d7e5067
Shintaro Sakoda <sakoda@keio.jp>
2025-05-20 10:58:47 +0900

    Fixed transformer_output
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- networks/sequence_compressor.py
+++ networks/sequence_compressor.py
@@ -210,7 +210,8 @@ class SequenceCompressor(nn.Module):
         x += self.pos_embedding
 
         # Transformer Encoderに通す
-        transformer_output = self.transformer_encoder(x)  # (batch_size, seq_len, hidden_dim)
+        # transformer_output = self.transformer_encoder(x)  # (batch_size, seq_len, hidden_dim)
+        transformer_output = x
 
         # 最後のトークンの出力を圧縮表現とする
         compressed_representation = transformer_output[:, -1, :]  # (batch_size, hidden_dim)
