822ed6876ef63e812f4ed0bb64d3a2683162b862
Shintaro Sakoda <sakoda@keio.jp>
2025-05-22 15:17:10 +0900

    Implemented the instance norm
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- networks/backbone.py
+++ networks/backbone.py
@@ -8,16 +8,22 @@ class BaseCNN(nn.Module):
         # input shape: (batch_size, in_channels, 96, 96)
         self.features = nn.Sequential(
             nn.Conv2d(in_channels, 8, kernel_size=4, stride=2),  # -> (8, 47, 47)
+            nn.GroupNorm(1, 8, affine=False),
             nn.ReLU(),
             nn.Conv2d(8, 16, kernel_size=3, stride=2),  # -> (16, 23, 23)
+            nn.GroupNorm(1, 16, affine=False),
             nn.ReLU(),
             nn.Conv2d(16, 32, kernel_size=3, stride=2),  # -> (32, 11, 11)
+            nn.GroupNorm(1, 32, affine=False),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, stride=2),  # -> (64, 5, 5)
+            nn.GroupNorm(1, 64, affine=False),
             nn.ReLU(),
             nn.Conv2d(64, 128, kernel_size=3, stride=1),  # -> (128, 3, 3)
+            nn.GroupNorm(1, 128, affine=False),
             nn.ReLU(),
             nn.Conv2d(128, 256, kernel_size=3, stride=1),  # -> (256, 1, 1)
+            nn.GroupNorm(1, 256, affine=False),
             nn.ReLU(),
             nn.Flatten(),  # -> (256,)
         )
