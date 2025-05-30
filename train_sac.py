a1cd01e22249ecfb25a14fb6a0a1b1008aefe51a
sakoda <sakoda@keio.jp>
2025-05-31 08:11:31 +0900

    Added episode loop
    Signed-off-by: sakoda <sakoda@keio.jp>

--- train_sac.py
+++ train_sac.py
@@ -127,178 +127,189 @@ if __name__ == "__main__":
     start_time = time.time()
 
     # start the game
-    episode_id = 0
+    global_step = 0
     score_list = []
     obs, _ = env.reset(seed=seed)
     progress_bar = tqdm(range(args.learning_starts), dynamic_ncols=True)
-    curr_image_dir = image_dir / f"ep_{episode_id:08d}"
-    curr_image_dir.mkdir(parents=True, exist_ok=True)
-    for global_step in range(args.total_timesteps):
-        # put action logic here
-        obs_tensor = torch.Tensor(obs).to(device).unsqueeze(0)
-        output_enc = encoder.encode(obs_tensor).detach()
-        output_dec = encoder.decode(output_enc).detach()
-        if global_step < args.learning_starts:
-            action = env.action_space.sample()
-            progress_bar.update(1)
-        else:
-            output_enc = output_enc.flatten(start_dim=1)
-            action, selected_log_pi, _ = actor.get_action(output_enc)
-            action = action[0].detach().cpu().numpy()
-            action = action * action_scale + action_bias
+    curr_image_dir = None
 
-            action_noise = env.action_space.sample()
-            c = args.action_noise
-            action = (1 - c) * action + c * action_noise
-            action = np.clip(action, action_low, action_high)
+    for episode_id in range(100000):
+        if (episode_id + 1) % image_save_interval == 0:
+            curr_image_dir = image_dir / f"ep_{episode_id:08d}"
+            curr_image_dir.mkdir(parents=True, exist_ok=True)
 
-        # execute the game and log data.
-        next_obs, reward, termination, truncation, info = env.step(action)
-        reward = np.clip(reward, -3.0, 3.0)
-        rb.add(obs, next_obs, action, reward, termination or truncation)
+        obs, _ = env.reset()
+        reward_list = []
+        first_value = None
 
-        # render
-        if args.render:
-            output_dec_display = output_dec[0].detach().cpu().numpy()  # (3, 96, 96)
-            observation_img = np.transpose(obs, (1, 2, 0))  # (96, 96, 3)
-            reconstructed_img = np.transpose(output_dec_display, (1, 2, 0))  # (96, 96, 3)
-            bgr_array = concat_images(env.render(), observation_img, reconstructed_img)
-            cv2.imshow("CarRacing", bgr_array)
-            cv2.waitKey(1)
+        while True:
+            global_step += 1
 
-        # save images for specific episodes
-        if episode_id % image_save_interval == 0 and curr_image_dir is not None:
-            output_dec_display = output_dec[0].detach().cpu().numpy()  # (3, 96, 96)
-            observation_img = np.transpose(obs, (1, 2, 0))  # (96, 96, 3)
-            reconstructed_img = np.transpose(output_dec_display, (1, 2, 0))  # (96, 96, 3)
-            bgr_array = concat_images(env.render(), observation_img, reconstructed_img)
-            cv2.imwrite(str(curr_image_dir / f"{global_step:08d}.png"), bgr_array)
-
-        if termination or truncation:
-            score = info["episode"]["r"]
-            score_list.append(score)
-            score_list = score_list[-20:]
-            recent_average_score = np.mean(score_list)
-
-            data_dict = {
-                "global_step": global_step,
-                "episodic_return": info["episode"]["r"],
-                "episodic_length": info["episode"]["l"],
-                "recent_average_score": recent_average_score,
-            }
-            wandb.log(data_dict)
-
-            log_episode.append(data_dict)
-            log_episode_df = pd.DataFrame(log_episode)
-            log_episode_df.to_csv(result_dir / "log_episode.tsv", sep="\t", index=False)
-
-            is_solved = recent_average_score > env.spec.reward_threshold
-
-            if episode_id % 10 == 0 or is_solved:
-                print(
-                    f"Ep: {episode_id}\tStep: {global_step}\tLast score: {score:.2f}\tAverage score: {recent_average_score:.2f}\tLength: {info['episode']['l']:.2f}"
-                )
-
-            # setup image directory for next episode if needed
-            if (episode_id + 1) % image_save_interval == 0:
-                curr_image_dir = image_dir / f"ep_{episode_id + 1:08d}"
-                curr_image_dir.mkdir(parents=True, exist_ok=True)
+            # select action
+            obs_tensor = torch.Tensor(obs).to(device).unsqueeze(0)
+            output_enc = encoder.encode(obs_tensor).detach()
+            output_dec = encoder.decode(output_enc).detach()
+            if global_step < args.learning_starts:
+                action = env.action_space.sample()
+                progress_bar.update(1)
             else:
-                curr_image_dir = None
+                output_enc = output_enc.flatten(start_dim=1)
+                action, selected_log_pi, _ = actor.get_action(output_enc)
+                action = action[0].detach().cpu().numpy()
+                action = action * action_scale + action_bias
 
-            episode_id += 1
+                action_noise = env.action_space.sample()
+                c = args.action_noise
+                action = (1 - c) * action + c * action_noise
+                action = np.clip(action, action_low, action_high)
 
-            if is_solved:
-                print(
-                    f"Solved! Running reward is now {recent_average_score} and the last episode runs to {score}!"
-                )
+            # execute the game and log data.
+            next_obs, reward, termination, truncation, info = env.step(action)
+            reward = np.clip(reward, -3.0, 3.0)
+            rb.add(obs, next_obs, action, reward, termination or truncation)
+
+            # render
+            if args.render:
+                output_dec_display = output_dec[0].detach().cpu().numpy()  # (3, 96, 96)
+                observation_img = np.transpose(obs, (1, 2, 0))  # (96, 96, 3)
+                reconstructed_img = np.transpose(output_dec_display, (1, 2, 0))  # (96, 96, 3)
+                bgr_array = concat_images(env.render(), observation_img, reconstructed_img)
+                cv2.imshow("CarRacing", bgr_array)
+                cv2.waitKey(1)
+
+            # save images for specific episodes
+            if episode_id % image_save_interval == 0 and curr_image_dir is not None:
+                output_dec_display = output_dec[0].detach().cpu().numpy()  # (3, 96, 96)
+                observation_img = np.transpose(obs, (1, 2, 0))  # (96, 96, 3)
+                reconstructed_img = np.transpose(output_dec_display, (1, 2, 0))  # (96, 96, 3)
+                bgr_array = concat_images(env.render(), observation_img, reconstructed_img)
+                cv2.imwrite(str(curr_image_dir / f"{global_step:08d}.png"), bgr_array)
+
+            if termination or truncation:
                 break
 
-            obs, _ = env.reset()
-        else:
             obs = next_obs
 
-        if global_step <= args.learning_starts:
-            continue
+            if global_step <= args.learning_starts:
+                continue
 
-        if global_step % 10 != 0:
-            continue
+            if global_step % 10 != 0:
+                continue
 
-        # training.
-        data = rb.sample(args.batch_size)
-        with torch.no_grad():
-            state_next = encoder.encode(data.next_observations)
-            state_next = state_next.flatten(start_dim=1)
-            next_state_actions, next_state_log_pi, _ = actor.get_action(state_next)
-            qf1_next_target = qf1(state_next, next_state_actions)
-            qf2_next_target = qf2(state_next, next_state_actions)
-            min_q = torch.min(qf1_next_target, qf2_next_target)
-            min_qf_next_target = min_q - alpha * next_state_log_pi
-            next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (
-                min_qf_next_target
-            ).view(-1)
+            # training.
+            data = rb.sample(args.batch_size)
+            with torch.no_grad():
+                state_next = encoder.encode(data.next_observations)
+                state_next = state_next.flatten(start_dim=1)
+                next_state_actions, next_state_log_pi, _ = actor.get_action(state_next)
+                qf1_next_target = qf1(state_next, next_state_actions)
+                qf2_next_target = qf2(state_next, next_state_actions)
+                min_q = torch.min(qf1_next_target, qf2_next_target)
+                min_qf_next_target = min_q - alpha * next_state_log_pi
+                next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (
+                    min_qf_next_target
+                ).view(-1)
 
-        state_curr = encoder.encode(data.observations).detach()
-        state_curr = state_curr.flatten(start_dim=1)
-        qf1_a_values = qf1(state_curr, data.actions).view(-1)
-        qf2_a_values = qf2(state_curr, data.actions).view(-1)
-        qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-        qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
-        qf_loss = qf1_loss + qf2_loss
+            state_curr = encoder.encode(data.observations).detach()
+            state_curr = state_curr.flatten(start_dim=1)
+            qf1_a_values = qf1(state_curr, data.actions).view(-1)
+            qf2_a_values = qf2(state_curr, data.actions).view(-1)
+            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+            qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
+            qf_loss = qf1_loss + qf2_loss
 
-        # optimize the model
-        q_optimizer.zero_grad()
-        qf_loss.backward()
-        q_optimizer.step()
+            # optimize the model
+            q_optimizer.zero_grad()
+            qf_loss.backward()
+            q_optimizer.step()
 
-        state_curr = encoder.encode(data.observations).detach()
-        state_curr = state_curr.flatten(start_dim=1)
-        pi, log_pi, _ = actor.get_action(state_curr)
-        qf1_pi = qf1(state_curr, pi)
-        qf2_pi = qf2(state_curr, pi)
-        min_qf_pi = torch.min(qf1_pi, qf2_pi)
-        actor_loss = ((alpha * log_pi) - min_qf_pi).mean()
+            state_curr = encoder.encode(data.observations).detach()
+            state_curr = state_curr.flatten(start_dim=1)
+            pi, log_pi, _ = actor.get_action(state_curr)
+            qf1_pi = qf1(state_curr, pi)
+            qf2_pi = qf2(state_curr, pi)
+            min_qf_pi = torch.min(qf1_pi, qf2_pi)
+            actor_loss = ((alpha * log_pi) - min_qf_pi).mean()
 
-        actor_optimizer.zero_grad()
-        actor_loss.backward()
-        actor_optimizer.step()
+            actor_optimizer.zero_grad()
+            actor_loss.backward()
+            actor_optimizer.step()
 
-        alpha_loss = (-log_alpha.exp() * (log_pi.detach() + target_entropy)).mean()
+            alpha_loss = (-log_alpha.exp() * (log_pi.detach() + target_entropy)).mean()
 
-        a_optimizer.zero_grad()
-        alpha_loss.backward()
-        a_optimizer.step()
-        alpha = log_alpha.exp().item() if args.fixed_alpha is None else args.fixed_alpha
+            a_optimizer.zero_grad()
+            alpha_loss.backward()
+            a_optimizer.step()
+            alpha = log_alpha.exp().item() if args.fixed_alpha is None else args.fixed_alpha
 
-        if global_step % 10 == 0:
-            elapsed_time = time.time() - start_time
-            data_dict = {
-                "global_step": global_step,
-                "losses/qf1_values": qf1_a_values.mean().item(),
-                "losses/qf2_values": qf2_a_values.mean().item(),
-                "losses/qf1_loss": qf1_loss.item(),
-                "losses/qf2_loss": qf2_loss.item(),
-                "losses/qf_loss": qf_loss.item() / 2.0,
-                "losses/min_qf_next_target": min_qf_next_target.mean().item(),
-                "losses/next_q_value": next_q_value.mean().item(),
-                "losses/actor_loss": actor_loss.item(),
-                "losses/alpha": alpha,
-                "losses/log_pi": log_pi.mean().item(),
-                "a_logp": selected_log_pi.mean().item(),
-                "losses/alpha_loss": alpha_loss.item(),
-                "charts/elapse_time_sec": elapsed_time,
-                "charts/SPS": int(global_step / elapsed_time),
-                "reward": reward,
-            }
-            wandb.log(data_dict)
+            if global_step % 10 == 0:
+                elapsed_time = time.time() - start_time
+                data_dict = {
+                    "global_step": global_step,
+                    "losses/qf1_values": qf1_a_values.mean().item(),
+                    "losses/qf2_values": qf2_a_values.mean().item(),
+                    "losses/qf1_loss": qf1_loss.item(),
+                    "losses/qf2_loss": qf2_loss.item(),
+                    "losses/qf_loss": qf_loss.item() / 2.0,
+                    "losses/min_qf_next_target": min_qf_next_target.mean().item(),
+                    "losses/next_q_value": next_q_value.mean().item(),
+                    "losses/actor_loss": actor_loss.item(),
+                    "losses/alpha": alpha,
+                    "losses/log_pi": log_pi.mean().item(),
+                    "a_logp": selected_log_pi.mean().item(),
+                    "losses/alpha_loss": alpha_loss.item(),
+                    "charts/elapse_time_sec": elapsed_time,
+                    "charts/SPS": int(global_step / elapsed_time),
+                    "reward": reward,
+                }
+                wandb.log(data_dict)
 
-            fixed_data = {
-                k.replace("losses/", "").replace("charts/", ""): v for k, v in data_dict.items()
-            }
-            log_step.append(fixed_data)
-            log_step_df = pd.DataFrame(log_step)
-            log_step_df.to_csv(
-                result_dir / "log_step.tsv", sep="\t", index=False, float_format="%.3f"
+                fixed_data = {
+                    k.replace("losses/", "").replace("charts/", ""): v for k, v in data_dict.items()
+                }
+                log_step.append(fixed_data)
+                log_step_df = pd.DataFrame(log_step)
+                log_step_df.to_csv(
+                    result_dir / "log_step.tsv", sep="\t", index=False, float_format="%.3f"
+                )
+
+        score = info["episode"]["r"]
+        score_list.append(score)
+        score_list = score_list[-20:]
+        recent_average_score = np.mean(score_list)
+
+        data_dict = {
+            "global_step": global_step,
+            "episodic_return": info["episode"]["r"],
+            "episodic_length": info["episode"]["l"],
+            "recent_average_score": recent_average_score,
+        }
+        wandb.log(data_dict)
+
+        log_episode.append(data_dict)
+        log_episode_df = pd.DataFrame(log_episode)
+        log_episode_df.to_csv(result_dir / "log_episode.tsv", sep="\t", index=False)
+
+        is_solved = recent_average_score > env.spec.reward_threshold
+
+        if episode_id % 10 == 0 or is_solved:
+            print(
+                f"Ep: {episode_id}\tStep: {global_step}\tLast score: {score:.2f}\tAverage score: {recent_average_score:.2f}\tLength: {info['episode']['l']:.2f}"
             )
+
+        # setup image directory for next episode if needed
+        if (episode_id + 1) % image_save_interval == 0:
+            curr_image_dir = image_dir / f"ep_{episode_id + 1:08d}"
+            curr_image_dir.mkdir(parents=True, exist_ok=True)
+        else:
+            curr_image_dir = None
+
+        episode_id += 1
+
+        if is_solved:
+            print(
+                f"Solved! Running reward is now {recent_average_score} and the last episode runs to {score}!"
+            )
+            break
 
     env.close()
