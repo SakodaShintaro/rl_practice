e59b18250c6b134cd775bad0993ee9e4b85d4b92
Shintaro Sakoda <sakoda@keio.jp>
2025-07-19 16:01:12 +0900

    Removed unused args
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- networks/backbone.py
+++ networks/backbone.py
@@ -265,9 +265,7 @@ class MMMambaEncoder:
         images = self.transform(images).to(device).to(torch.bfloat16)
         model_inputs = self.tokenizer(
             text=["Please describe" + "<IMG_CONTEXT>" * 256] * batch_size,
-            images=images,
             return_tensors="pt",
-            do_rescale=False,
             padding=True,
         )
         input_ids = model_inputs["input_ids"].to(device)
@@ -283,70 +281,6 @@ class MMMambaEncoder:
 
     def forward(self, x):
         return self.encode(x)
-
-    def find_closest_aspect_ratio(self, aspect_ratio, target_ratios, width, height, image_size):
-        best_ratio_diff = float("inf")
-        best_ratio = (1, 1)
-        area = width * height
-        for ratio in target_ratios:
-            target_aspect_ratio = ratio[0] / ratio[1]
-            ratio_diff = abs(aspect_ratio - target_aspect_ratio)
-            if ratio_diff < best_ratio_diff:
-                best_ratio_diff = ratio_diff
-                best_ratio = ratio
-            elif ratio_diff == best_ratio_diff:
-                if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
-                    best_ratio = ratio
-        return best_ratio
-
-    def dynamic_preprocess(self, image, min_num=1, max_num=12, image_size=448, use_thumbnail=False):
-        orig_width, orig_height = image.size
-        aspect_ratio = orig_width / orig_height
-        # calculate the existing image aspect ratio
-        target_ratios = set(
-            (i, j)
-            for n in range(min_num, max_num + 1)
-            for i in range(1, n + 1)
-            for j in range(1, n + 1)
-            if i * j <= max_num and i * j >= min_num
-        )
-        target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
-        # find the closest aspect ratio to the target
-        target_aspect_ratio = self.find_closest_aspect_ratio(
-            aspect_ratio, target_ratios, orig_width, orig_height, image_size
-        )
-        # calculate the target width and height
-        target_width = image_size * target_aspect_ratio[0]
-        target_height = image_size * target_aspect_ratio[1]
-        blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
-        # resize the image
-        resized_img = image.resize((target_width, target_height))
-        processed_images = []
-        for i in range(blocks):
-            box = (
-                (i % (target_width // image_size)) * image_size,
-                (i // (target_width // image_size)) * image_size,
-                ((i % (target_width // image_size)) + 1) * image_size,
-                ((i // (target_width // image_size)) + 1) * image_size,
-            )
-            # split the image
-            split_img = resized_img.crop(box)
-            processed_images.append(split_img)
-        assert len(processed_images) == blocks
-        if use_thumbnail and len(processed_images) != 1:
-            thumbnail_img = image.resize((image_size, image_size))
-            processed_images.append(thumbnail_img)
-        return processed_images
-
-    def load_image(self, image_file, input_size=448, max_num=12):
-        image = Image.open(image_file).convert("RGB")
-        transform = self.build_transform(input_size=input_size)
-        images = self.dynamic_preprocess(
-            image, image_size=input_size, use_thumbnail=True, max_num=max_num
-        )
-        pixel_values = [transform(image) for image in images]
-        pixel_values = torch.stack(pixel_values)
-        return pixel_values
 
 
 if __name__ == "__main__":
