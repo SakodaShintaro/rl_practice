e001c339973c40eb826f9cbcbcfc758caab69472
Shintaro Sakoda <sakoda@keio.jp>
2025-07-21 14:25:27 +0900

    Removed duplicated mmMambaRMSNorm and mmMambaRotaryEmbedding
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- networks/for_mmmamba/modeling_mmMamba_embedding.py
+++ networks/for_mmmamba/modeling_mmMamba_embedding.py
@@ -28,7 +28,13 @@ from transformers.utils import logging
 compute_ARank = False  # [ARank] Set this to True to compute attention rank
 
 from .configuration_mmMamba_embedding import mmMambaEmbeddingConfig
-from .modeling_mmMamba import MHA_LM, Mamba2_LM, mmMambaDecoderLayer
+from .modeling_mmMamba import (
+    MHA_LM,
+    Mamba2_LM,
+    mmMambaDecoderLayer,
+    mmMambaRMSNorm,
+    mmMambaRotaryEmbedding,
+)
 
 try:
     from flash_attn import flash_attn_with_kvcache
@@ -93,65 +99,6 @@ def _update_kv_cache(kv, inference_params, layer_idx):
     assert kv_cache is not None
     kv_cache[batch_start:batch_end, sequence_start:sequence_end, ...] = kv
     return kv_cache[batch_start:batch_end, :sequence_end, ...]
-
-
-# Copied from transformers.models.llama.modeling_llama.LlamaRMSNorm with Llama->mmMamba
-class mmMambaRMSNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-6):
-        """
-        mmMambaRMSNorm is equivalent to T5LayerNorm
-        """
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = eps
-
-    def forward(self, hidden_states):
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
-
-
-# Copied from transformers.model.llama.modeling_llama.LlamaRotaryEmbedding with Llama->mmMamba
-class mmMambaRotaryEmbedding(nn.Module):
-    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
-        super().__init__()
-
-        self.dim = dim
-        self.max_position_embeddings = max_position_embeddings
-        self.base = base
-        inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2).float().to(device) / self.dim))
-        self.register_buffer("inv_freq", inv_freq, persistent=False)
-
-        # Build here to make `torch.jit.trace` work.
-        self._set_cos_sin_cache(
-            seq_len=max_position_embeddings,
-            device=self.inv_freq.device,
-            dtype=torch.get_default_dtype(),
-        )
-
-    def _set_cos_sin_cache(self, seq_len, device, dtype):
-        self.max_seq_len_cached = seq_len
-        t = torch.arange(self.max_seq_len_cached, device=device, dtype=self.inv_freq.dtype)
-
-        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-        # Different from paper, but it uses a different permutation in order to obtain the same calculation
-        emb = torch.cat((freqs, freqs), dim=-1)
-        self.cos_cached = emb.cos().to(dtype)
-        self.sin_cached = emb.sin().to(dtype)
-        # self.register_buffer("cos_cached", emb.cos().to(dtype), persistent=False)
-        # self.register_buffer("sin_cached", emb.sin().to(dtype), persistent=False)
-
-    def forward(self, x, seq_len=None):
-        # x: [bs, num_attention_heads, seq_len, head_size]
-        if seq_len > self.max_seq_len_cached:
-            self._set_cos_sin_cache(seq_len=seq_len, device=x.device, dtype=torch.float32)
-
-        return (
-            self.cos_cached[:seq_len].to(dtype=x.dtype),
-            self.sin_cached[:seq_len].to(dtype=x.dtype),
-        )
 
 
 # Copied from transformers.model.llama.modeling_llama.LlamaLinearScalingRotaryEmbedding with Llama->mmMamba
