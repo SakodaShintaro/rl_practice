d8f98bc484f3d613b83ac8aba23cef2d073e11d7
Shintaro Sakoda <sakoda@keio.jp>
2025-07-23 21:03:55 +0900

    Renamed from "`train_sac.py` to `train.py`
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
 
-python3 train_sac.py test --debug
-python3 train_sac.py test --debug --agent_type avg
-python3 train_sac.py test --debug --agent_type ppo
+python3 train.py test --debug
+python3 train.py test --debug --agent_type avg
+python3 train.py test --debug --agent_type ppo
