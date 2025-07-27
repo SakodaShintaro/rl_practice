Fixed AverageRewardEarlyStopWrapper
Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

agents/avg.py
@@ -170,9 +170,6 @@ class AvgAgent:
         info_dict["action_norm"] = action_norm
         info_dict["train_reward"] = train_reward
 
-        # done = termination or truncation  # こちらの方が強化学習の理論的には正しい
-        done = False  # しかし実践的には性能が悪くなるので、Falseに固定
-
         obs_tensor = torch.Tensor(obs).unsqueeze(0).to(self.device)
         curr_obs = self.network.encoder_image.encode(obs_tensor)
 
@@ -189,7 +186,7 @@ class AvgAgent:
         rewards = torch.tensor(
             [[train_reward, train_reward]], device=self.device, dtype=torch.float32
         )
-        dones = torch.tensor([[done, done]], device=self.device, dtype=torch.float32)
+        dones = torch.tensor([[termination, termination]], device=self.device, dtype=torch.float32)
 
         data = ReplayBufferData(
             observations=observations, actions=actions, rewards=rewards, dones=dones
@@ -225,7 +222,7 @@ class AvgAgent:
 
         if self.use_eligibility_trace:
             delta = critic_info["delta"]
-            self.optimizer.step(delta, reset=done)
+            self.optimizer.step(delta, reset=(termination or truncation))
         else:
             self.optimizer.step()
 