d3eb95f6c5dc8952cf0a1b4c77a18687d0e0f51d
Shintaro Sakoda <sakoda@keio.jp>
2025-08-02 14:56:28 +0900

    Added play_zeroshot.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ play_zeroshot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eux
+
+python3 play_zeroshot.py --render=0
+python3 play_zeroshot.py --render=0 --agent_type qwenvl
+python3 play_zeroshot.py --render=0 --agent_type smolvlm
+python3 play_zeroshot.py --render=0 --agent_type mmmamba
