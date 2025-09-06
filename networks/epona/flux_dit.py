b5eec308cc0090181ab43afae51ddf08400f93fc
Shintaro Sakoda <sakoda@keio.jp>
2025-09-06 11:28:53 +0900

    Removed mean_flat
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- networks/epona/flux_dit.py
+++ networks/epona/flux_dit.py
@@ -13,13 +13,6 @@ from .layers import (
 )
 
 
-def mean_flat(tensor):
-    """
-    Take the mean over all non-batch dimensions.
-    """
-    return tensor.mean(dim=list(range(1, len(tensor.shape))))
-
-
 @dataclass
 class FluxParams:
     in_channels: int
