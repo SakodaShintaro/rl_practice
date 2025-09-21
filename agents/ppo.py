893181182390006e42188869199a224f189c0f64
Shintaro Sakoda <sakoda@keio.jp>
2025-09-22 08:38:39 +0900

    Moved PpoBetaPolicyAndValue
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- agents/ppo.py
+++ agents/ppo.py
@@ -2,8 +2,63 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
+from torch.distributions import Beta
 
-from networks.ppo_beta_policy_and_value import PpoBetaPolicyAndValue
+from networks.backbone import RecurrentEncoder, SingleFrameEncoder
+
+
+class PpoBetaPolicyAndValue(nn.Module):
+    def __init__(self, action_dim: int, seq_len: int, encoder_type: str) -> None:
+        super().__init__()
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        if encoder_type == "recurrent":
+            self.encoder = RecurrentEncoder(seq_len)
+        else:
+            self.encoder = SingleFrameEncoder(seq_len, device)
+        seq_hidden_dim = self.encoder.output_dim
+        rep_dim = 256
+        hidden_dim = 100
+        self.linear = nn.Linear(seq_hidden_dim, rep_dim)
+        self.norm = nn.RMSNorm(rep_dim, elementwise_affine=False)
+        self.value_enc = nn.Sequential(nn.Linear(rep_dim, hidden_dim), nn.ReLU())
+        self.value_head = nn.Linear(hidden_dim, 1)
+        self.policy_enc = nn.Sequential(nn.Linear(rep_dim, hidden_dim), nn.ReLU())
+        self.alpha_head = nn.Linear(hidden_dim, action_dim)
+        self.beta_head = nn.Linear(hidden_dim, action_dim)
+
+    def forward(
+        self,
+        r_seq: torch.Tensor,
+        s_seq: torch.Tensor,
+        a_seq: torch.Tensor,
+        action: torch.Tensor | None = None,
+    ) -> tuple:
+        # (B, T, C, H, W), (B, T, action_dim), (B, T)
+        x = self.encoder(s_seq, a_seq, r_seq)
+        x = self.linear(x)  # (batch_size, rep_dim)
+        x = self.norm(x)
+
+        value_x = self.value_enc(x)
+        value = self.value_head(value_x)
+
+        policy_x = self.policy_enc(x)
+        alpha = self.alpha_head(policy_x).exp() + 1
+        beta = self.beta_head(policy_x).exp() + 1
+
+        dist = Beta(alpha, beta)
+        if action is None:
+            action = dist.sample()
+        a_logp = dist.log_prob(action).sum(dim=1, keepdim=True)
+
+        return {
+            "action": action,
+            "a_logp": a_logp,
+            "value": value,
+            "x": x,
+            "value_x": value_x,
+            "policy_x": policy_x,
+        }
 
 
 class SequentialBatchSampler:
