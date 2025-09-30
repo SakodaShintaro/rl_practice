94b23917c41465a4bef852ee53f9601d31af7fdc
Shintaro Sakoda <sakoda@keio.jp>
2025-10-01 06:20:53 +0900

    Fixed default seq_len to 32
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--sparsity", type=float, default=0.0)
     parser.add_argument("--gamma", type=float, default=0.99)
     parser.add_argument("--step_limit", type=int, default=10_000_000)
-    parser.add_argument("--seq_len", type=int, default=8)
+    parser.add_argument("--seq_len", type=int, default=32)
     parser.add_argument("--action_norm_penalty", type=float, default=0.0)
     parser.add_argument("--render_reconstruction", type=int, default=0, choices=[0, 1])
     parser.add_argument(
