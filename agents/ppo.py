b83017d69773a77702a05e6239b1fea798e7162f
Shintaro Sakoda <sakoda@keio.jp>
2025-10-04 22:07:59 +0900

    Fixed ppo.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- agents/ppo.py
+++ agents/ppo.py
@@ -197,83 +197,13 @@ class PpoAgent:
         self.episode_logps = []
         self.episode_rnn_states = []
 
-    def select_action(self, global_step, obs, reward: float) -> tuple[np.ndarray, dict]:
-        episode_reward = getattr(self, "episode_reward", 0.0)
-        action, a_logp, value, result_dict = self._select_action_original(episode_reward, obs)
-
-        action_info = {
-            "a_logp": a_logp,
-            "value": value,
-            "reward": reward,
-            "normed_reward": reward / 10.0,
-        }
-
-        for key in ["x", "value_x", "policy_x"]:
-            if key in result_dict:
-                value_tensor = result_dict[key]
-                action_info[f"activation/{key}_norm"] = value_tensor.norm(dim=1).mean().item()
-                action_info[f"activation/{key}_mean"] = value_tensor.mean(dim=1).mean().item()
-                action_info[f"activation/{key}_std"] = value_tensor.std(dim=1).mean().item()
-
-        self.episode_states.append(obs)
-        self.episode_actions.append(action)
-        self.episode_values.append(value)
-        self.episode_logps.append(a_logp)
-        self.episode_rnn_states.append(self.rnn_state.cpu().numpy())
-
-        return action, action_info
-
-    def step(self, global_step, obs, reward, termination, truncation) -> tuple[np.ndarray, dict]:
-        self.episode_reward += reward
-        normed_reward = reward / 10.0
-
-        info_dict = {}
-
-        if len(self.episode_states) > 0:
-            prev_obs = self.episode_states[-1]
-            prev_action = self.episode_actions[-1]
-            prev_value = self.episode_values[-1]
-            prev_logp = self.episode_logps[-1]
-            prev_rnn_state = self.episode_rnn_states[-1]
-
-            self.buffer[self.counter] = (
-                prev_obs,
-                prev_action,
-                prev_logp,
-                normed_reward,
-                prev_value,
-                False,
-                prev_rnn_state,
-            )
-            self.counter += 1
-            if self.counter == self.buffer_capacity:
-                train_result = self._update()
-                info_dict.update(train_result)
-                self.counter = 0
-
-        if termination or truncation:
-            # エピソード終了時の統計情報を追加
-            if len(self.episode_values) > 0:
-                info_dict["first_value"] = self.episode_values[0]
-                info_dict["weighted_reward"] = getattr(self, "episode_reward", 0.0)
-
-        # make decision
-        action, action_info = self.select_action(global_step, obs, reward)
-        info_dict.update(action_info)
-
-        return action, info_dict
-
-    ####################
-    # Internal methods #
-    ####################
-
     @torch.inference_mode()
-    def _select_action_original(self, reward: float, state: np.ndarray) -> tuple:
+    def select_action(self, global_step, obs, reward: float) -> tuple[np.ndarray, dict]:
         reward = torch.from_numpy(np.array(reward)).to(self.device).unsqueeze(0)
-        state = torch.from_numpy(state).to(self.device).unsqueeze(0)
+        obs_t = torch.from_numpy(obs).to(self.device).unsqueeze(0)
         self.r_list.append(reward)
         self.r_list = self.r_list[-self.seq_len :]
-        self.s_list.append(state)
+        self.s_list.append(obs_t)
         self.s_list = self.s_list[-self.seq_len :]
 
         curr_r = torch.cat(self.r_list, dim=0).unsqueeze(0).unsqueeze(-1)
@@ -311,7 +241,68 @@ class PpoAgent:
         action = action * self.action_scale + self.action_bias
         action = np.clip(action, self.action_low, self.action_high)
 
-        return action, a_logp, value, result_dict
+        action_info = {
+            "a_logp": a_logp,
+            "value": value,
+        }
+
+        for key in ["x", "value_x", "policy_x"]:
+            if key in result_dict:
+                value_tensor = result_dict[key]
+                action_info[f"activation/{key}_norm"] = value_tensor.norm(dim=1).mean().item()
+                action_info[f"activation/{key}_mean"] = value_tensor.mean(dim=1).mean().item()
+                action_info[f"activation/{key}_std"] = value_tensor.std(dim=1).mean().item()
+
+        self.episode_states.append(obs)
+        self.episode_actions.append(action)
+        self.episode_values.append(value)
+        self.episode_logps.append(a_logp)
+        self.episode_rnn_states.append(self.rnn_state.cpu().numpy())
+
+        return action, action_info
+
+    def step(self, global_step, obs, reward, termination, truncation) -> tuple[np.ndarray, dict]:
+        self.episode_reward += reward
+
+        info_dict = {}
+
+        if len(self.episode_states) > 0:
+            prev_obs = self.episode_states[-1]
+            prev_action = self.episode_actions[-1]
+            prev_value = self.episode_values[-1]
+            prev_logp = self.episode_logps[-1]
+            prev_rnn_state = self.episode_rnn_states[-1]
+
+            self.buffer[self.counter] = (
+                prev_obs,
+                prev_action,
+                prev_logp,
+                reward,
+                prev_value,
+                False,
+                prev_rnn_state,
+            )
+            self.counter += 1
+            if self.counter == self.buffer_capacity:
+                train_result = self._update()
+                info_dict.update(train_result)
+                self.counter = 0
+
+        if termination or truncation:
+            # エピソード終了時の統計情報を追加
+            if len(self.episode_values) > 0:
+                info_dict["first_value"] = self.episode_values[0]
+                info_dict["weighted_reward"] = getattr(self, "episode_reward", 0.0)
+
+        # make decision
+        action, action_info = self.select_action(global_step, obs, reward)
+        info_dict.update(action_info)
+
+        return action, info_dict
+
+    ####################
+    # Internal methods #
+    ####################
 
     def _update(self) -> None:
         self.training_step += 1
@@ -350,11 +341,9 @@ class PpoAgent:
 
         ave_action_loss_list = []
         ave_value_loss_list = []
-        ave_pred_s_loss_list = []
         for _ in range(self.ppo_epoch):
             sum_action_loss = 0.0
             sum_value_loss = 0.0
-            sum_pred_s_loss = 0.0
             for indices in SequentialBatchSampler(
                 self.buffer_capacity - 1,
                 self.batch_size,
@@ -391,19 +380,9 @@ class PpoAgent:
                 value_loss_clipped = F.smooth_l1_loss(value_clipped, target_v[index])
                 value_loss = torch.max(value_loss_unclipped, value_loss_clipped)
 
-                if "error" in net_out_dict:
-                    pred_error = net_out_dict["error"]
-                    pred_error_s = pred_error[:, 3::3]  # 先頭は明らかに予測不可能なので3から
-                    pred_error_a = pred_error[:, 1::3]
-                    pred_error_r = pred_error[:, 2::3]
-                    pred_loss_s = pred_error_s.mean()
-                else:
-                    pred_loss_s = torch.tensor(0.0, device=self.device)
-
-                loss = action_loss + 2.0 * value_loss + pred_loss_s
+                loss = action_loss + 0.25 * value_loss
                 sum_action_loss += action_loss.item() * len(index)
                 sum_value_loss += value_loss.item() * len(index)
-                sum_pred_s_loss += pred_loss_s.item() * len(index)
 
                 self.optimizer.zero_grad()
                 loss.backward()
@@ -412,15 +391,12 @@ class PpoAgent:
 
             ave_action_loss = sum_action_loss / self.buffer_capacity
             ave_value_loss = sum_value_loss / self.buffer_capacity
-            ave_pred_s_loss = sum_pred_s_loss / self.buffer_capacity
             ave_action_loss_list.append(ave_action_loss)
             ave_value_loss_list.append(ave_value_loss)
-            ave_pred_s_loss_list.append(ave_pred_s_loss)
         result_dict = {}
         ratio_list = []
         result_dict["ppo/average_action_loss"] = np.mean(ave_action_loss_list)
         result_dict["ppo/average_value_loss"] = np.mean(ave_value_loss_list)
-        result_dict["ppo/average_pred_s_loss"] = np.mean(ave_pred_s_loss_list)
         result_dict["ppo/average_ratio"] = np.mean(ratio_list)
         result_dict["ppo/average_target_v"] = target_v.mean().item()
         result_dict["ppo/average_adv"] = adv.mean().item()
