4d52194406c15d33908ca96b359d67df89d78135
Shintaro Sakoda <sakoda@keio.jp>
2025-10-07 23:14:19 +0900

    Added StateValueHead
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- networks/value_head.py
+++ networks/value_head.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn.functional as F
 from torch import nn
 
 from .blocks import SimbaBlock
@@ -11,6 +10,40 @@ def weights_init_(m):
         nn.init.xavier_uniform_(m.weight, gain=1)
         # nn.init.orthogonal_(m.weight.data)
         nn.init.constant_(m.bias, 0)
+
+
+class StateValueHead(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_dim: int,
+        block_num: int,
+        num_bins: int,
+        sparsity: float,
+    ) -> None:
+        super().__init__()
+        self.fc_in = nn.Linear(in_channels, hidden_dim)
+        self.fc_mid = nn.Sequential(*[SimbaBlock(hidden_dim) for _ in range(block_num)])
+        self.norm = nn.LayerNorm(hidden_dim, elementwise_affine=False)
+        self.fc_out = nn.Linear(hidden_dim, num_bins)
+        self.apply(weights_init_)
+
+        self.sparse_mask = (
+            None if sparsity == 0.0 else apply_one_shot_pruning(self, overall_sparsity=sparsity)
+        )
+
+    def forward(self, x: torch.Tensor) -> dict[str, torch.Tensor]:
+        result_dict = {}
+
+        x = self.fc_in(x)
+        x = self.fc_mid(x)
+        x = self.norm(x)
+        result_dict["activation"] = x
+
+        output = self.fc_out(x)
+        result_dict["output"] = output
+
+        return result_dict
 
 
 class ActionValueHead(nn.Module):
