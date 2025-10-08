93d4a694d619bc43c5d96dffa669572adcc73ea8
sakoda <sakoda@keio.jp>
2025-10-08 22:14:55 +0900

    Fixed num_bins to 51
    Signed-off-by: sakoda <sakoda@keio.jp>

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--predictor_hidden_dim", type=int, default=512)
     parser.add_argument("--predictor_block_num", type=int, default=2)
     parser.add_argument("--predictor_step_num", type=int, default=1)
-    parser.add_argument("--num_bins", type=int, default=1)
+    parser.add_argument("--num_bins", type=int, default=51)
     parser.add_argument("--learning_rate", type=float, default=2e-4)
     parser.add_argument("--sparsity", type=float, default=0.0)
     parser.add_argument("--gamma", type=float, default=0.99)
