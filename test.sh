79d961e023e67c2a54d5f5e2c8104db141ffa686
Shintaro Sakoda <sakoda@keio.jp>
2025-10-09 09:52:55 +0900

    Fixed train and test
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eux
 
-python3 train.py test --debug
+python3 train.py test --debug --agent_type sac
 python3 train.py test --debug --agent_type avg
-python3 train.py test --debug --agent_type ppo --encoder=recurrent
-python3 train.py test --debug --encoder=single_frame
-python3 train.py test --debug --encoder=simple
+python3 train.py test --debug --agent_type ppo
--- train_car_racing.sh
+++ train_car_racing.sh
@@ -8,3 +8,6 @@ python3 train.py baseline \
   --action_norm_penalty 0.1 \
   --target_score 800.0 \
   --agent_type sac \
+  --seq_len 8 \
+  --num_bins 1 \
+  --learning_rate 1e-4 \
