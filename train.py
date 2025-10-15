15bd3d4c803547d9e48019608aeb2fcaa06da7df
sakoda <sakoda@keio.jp>
2025-10-16 05:53:19 +0900

    Fixed the default value_range
    Signed-off-by: sakoda <sakoda@keio.jp>

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--predictor_block_num", type=int, default=2)
     parser.add_argument("--predictor_step_num", type=int, default=1)
     parser.add_argument("--num_bins", type=int, default=51)
-    parser.add_argument("--value_range", type=int, default=1)
+    parser.add_argument("--value_range", type=float, default=2.0)
     parser.add_argument("--learning_rate", type=float, default=2e-4)
     parser.add_argument("--sparsity", type=float, default=0.0)
     parser.add_argument("--gamma", type=float, default=0.99)
--- train_car_racing.sh
+++ train_car_racing.sh
@@ -13,6 +13,7 @@ python3 train.py baseline \
   --tempo_block_type transformer \
   --seq_len 8 \
   --num_bins 1 \
+  --value_range 60.0 \
   --learning_rate 1e-4 \
   --step_limit 40_000 \
   --eval_range 20 \
