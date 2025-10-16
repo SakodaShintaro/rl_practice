a3fa4d35eae015576e71a171b1d09e9e91c043b6
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-10-16 21:34:57 +0900

    Fixed bias
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- networks/utils.py
+++ networks/utils.py
@@ -4,16 +4,19 @@ import torch.nn as nn
 def init_weights_xavier(m):
     if isinstance(m, nn.Linear):
         nn.init.xavier_uniform_(m.weight, gain=1)
-        nn.init.constant_(m.bias, 0)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
 
 
 def init_weights_orthogonal(m):
     if isinstance(m, nn.Linear):
         nn.init.orthogonal_(m.weight, gain=1)
-        nn.init.constant_(m.bias, 0)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
 
 
 def init_weights_normal(m):
     if isinstance(m, nn.Linear):
         nn.init.normal_(m.weight, mean=0.0, std=0.02)
-        nn.init.constant_(m.bias, 0)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
