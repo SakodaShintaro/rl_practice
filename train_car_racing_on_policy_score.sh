c89de79b1c2c479f5f392d132be6aa543c78082a
Shintaro Sakoda <sakoda@keio.jp>
2025-10-19 15:47:59 +0900

    Added train_car_racing_on_policy_score.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ train_car_racing_on_policy_score.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eux
+
+cd $(dirname $0)
+
+python3 train.py baseline \
+  --env_id CarRacing-v3 \
+  --agent_type ppo \
+  --use_action_value=0 \
+  --reward_scale 0.1 \
+  --action_norm_penalty 1.0 \
+  --buffer_size 20000 \
+  --learning_starts 2000 \
+  --target_score 800.0 \
+  --encoder spatial_temporal \
+  --tempo_block_type transformer \
+  --seq_len 8 \
+  --num_bins 1 \
+  --value_range 60.0 \
+  --learning_rate 1e-4 \
+  --step_limit 400_000 \
+  --eval_range 20 \
+  --disable_state_predictor=0 \
