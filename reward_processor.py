95aa8d5ae4000038641e3a569076bbe4e4cc411e
Shintaro Sakoda <sakoda@keio.jp>
2025-10-23 08:49:57 +0900

    Fixed reward_processor.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- reward_processor.py
+++ reward_processor.py
@@ -10,24 +10,40 @@ class RewardProcessor:
         self.epsilon = 1e-8
         self.type = processing_type
 
-    def symlog(self, x: float) -> float:
+    def _symlog(self, x: float) -> float:
         """Symmetric log."""
         return np.sign(x) * np.log(1 + np.abs(x))
 
     def normalize(self, reward: float) -> float:
         """Normalize the reward."""
         if self.type == "none":
-            return reward
+            result = reward
         elif self.type == "const":
-            return reward / 500.0
+            result = reward / 500.0
         elif self.type == "symlog":
-            return self.symlog(reward)
+            result = self._symlog(reward)
         elif self.type == "scaling":
             self.return_rms.update(np.array([reward]))
-            return reward / np.sqrt(self.return_rms.var + self.epsilon)
+            result = reward / np.sqrt(self.return_rms.var + self.epsilon)
         elif self.type == "centering":
             self.return_rms.update(np.array([reward]))
-            return (reward - self.return_rms.mean) / np.sqrt(self.return_rms.var + self.epsilon)
+            result = (reward - self.return_rms.mean) / np.sqrt(self.return_rms.var + self.epsilon)
         else:
             msg = "Invalid normalizer type"
             raise ValueError(msg)
+
+        MAX_VALUE = 10.0
+        result = np.clip(result, -MAX_VALUE, MAX_VALUE)
+        return result
+
+
+if __name__ == "__main__":
+    rp_symlog = RewardProcessor("symlog")
+    rp_scaling = RewardProcessor("scaling")
+    rp_centering = RewardProcessor("centering")
+    rewards = [10.0, 2.0, 3.0, 4.0, 5.0, -4.0, -10.0, 0.0, 1.0, -1.0]
+    for r in rewards:
+        norm_r_symlog = rp_symlog.normalize(r)
+        norm_r_scaling = rp_scaling.normalize(r)
+        norm_r_centering = rp_centering.normalize(r)
+        print(f"{r=:.3f}, {norm_r_symlog=:.3f}, {norm_r_scaling=:.3f}, {norm_r_centering=:.3f}")
