22a367989b28e825407cca5b066112d12d85e154
Shintaro Sakoda <sakoda@keio.jp>
2025-10-24 05:25:39 +0900

    Fixed train_minigrid_memory_off_policy_pathwise.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_minigrid_memory_off_policy_pathwise.sh
+++ train_minigrid_memory_off_policy_pathwise.sh
@@ -6,6 +6,7 @@ cd $(dirname $0)
 python3 train.py pathwise \
   --agent_type off_policy \
   --encoder temporal_only \
-  --temporal_model_type gru \
+  --temporal_model_type transformer \
   --step_limit 400_000 \
   --use_done=1 \
+  --denoising_time 0.8 \
