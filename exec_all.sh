edb00d161b6839b1602692226a00ef8498780e1c
sakoda <sakoda@keio.jp>
2025-10-28 07:26:27 +0900

    Reduce execution
    Signed-off-by: sakoda <sakoda@keio.jp>

--- exec_all.sh
+++ exec_all.sh
@@ -4,8 +4,8 @@ set -x
 cd $(dirname $0)
 
 ./train_car_racing_off_policy_pathwise.sh
-./train_car_racing_on_policy_pathwise.sh
-./train_car_racing_on_policy_score.sh
+# ./train_car_racing_on_policy_pathwise.sh
+# ./train_car_racing_on_policy_score.sh
 ./train_minigrid_memory_off_policy_pathwise.sh
-./train_minigrid_memory_on_policy_pathwise.sh
+# ./train_minigrid_memory_on_policy_pathwise.sh
 ./train_minigrid_memory_on_policy_score.sh
