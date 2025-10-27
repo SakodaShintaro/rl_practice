36c0acd08e8d1ee38207f58bb8e9530f977ec753
sakoda <sakoda@keio.jp>
2025-10-28 06:57:21 +0900

    Fixed reward_processor.py
    Signed-off-by: sakoda <sakoda@keio.jp>

--- reward_processor.py
+++ reward_processor.py
@@ -5,28 +5,27 @@ import numpy as np
 class RewardProcessor:
     """Reward Processor."""
 
-    def __init__(self, processing_type: str) -> None:
+    def __init__(self, processing_type: str, constant: float) -> None:
         self.return_rms = gym.wrappers.utils.RunningMeanStd(shape=())
         self.epsilon = 1e-8
         self.type = processing_type
+        self.constant = constant
 
-    def _symlog(self, x: float) -> float:
-        """Symmetric log."""
-        return np.sign(x) * np.log(1 + np.abs(x))
+    def update(self, reward: float) -> None:
+        """Update the running mean and std with the new reward."""
+        self.return_rms.update(np.array([reward]))
 
     def normalize(self, reward: float) -> float:
         """Normalize the reward."""
         if self.type == "none":
             result = reward
         elif self.type == "const":
-            result = reward / 500.0
+            result = reward / self.constant
         elif self.type == "symlog":
             result = self._symlog(reward)
         elif self.type == "scaling":
-            self.return_rms.update(np.array([reward]))
             result = reward / np.sqrt(self.return_rms.var + self.epsilon)
         elif self.type == "centering":
-            self.return_rms.update(np.array([reward]))
             result = (reward - self.return_rms.mean) / np.sqrt(self.return_rms.var + self.epsilon)
         else:
             msg = "Invalid normalizer type"
@@ -36,14 +35,34 @@ class RewardProcessor:
         result = np.clip(result, -MAX_VALUE, MAX_VALUE)
         return result
 
+    def inverse(self, reward: float) -> float:
+        """Inverse normalization of the reward."""
+        if self.type == "none":
+            result = reward
+        elif self.type == "const":
+            result = reward * self.constant
+        elif self.type == "scaling":
+            result = reward * np.sqrt(self.return_rms.var + self.epsilon)
+        elif self.type == "centering":
+            result = reward * np.sqrt(self.return_rms.var + self.epsilon) + self.return_rms.mean
+        else:
+            msg = "Invalid normalizer type"
+            raise ValueError(msg)
+
+        return result
+
 
 if __name__ == "__main__":
-    rp_symlog = RewardProcessor("symlog")
-    rp_scaling = RewardProcessor("scaling")
-    rp_centering = RewardProcessor("centering")
-    rewards = [10.0, 2.0, 3.0, 4.0, 5.0, -4.0, -10.0, 0.0, 1.0, -1.0]
+    rp_scaling = RewardProcessor("scaling", 0.0)
+    rp_centering = RewardProcessor("centering", 0.0)
+    rewards = [0.5, 10.0, 2.0, 3.0, 4.0, 5.0, -4.0, -10.0, 0.0, 1.0, -1.0]
     for r in rewards:
-        norm_r_symlog = rp_symlog.normalize(r)
+        rp_scaling.update(r)
+        rp_centering.update(r)
         norm_r_scaling = rp_scaling.normalize(r)
         norm_r_centering = rp_centering.normalize(r)
-        print(f"{r=:.3f}, {norm_r_symlog=:.3f}, {norm_r_scaling=:.3f}, {norm_r_centering=:.3f}")
+        inv_r_scaling = rp_scaling.inverse(norm_r_scaling)
+        inv_r_centering = rp_centering.inverse(norm_r_centering)
+        print(
+            f"{r=:+6.2f}, {norm_r_scaling=:+6.2f}, {norm_r_centering=:+6.2f} -> {inv_r_scaling=:+6.2f}, {inv_r_centering=:+6.2f}"
+        )
