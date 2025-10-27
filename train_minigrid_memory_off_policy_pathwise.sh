90e8225633767899ae860f589339e0d1163a2e88
Shintaro Sakoda <sakoda@keio.jp>
2025-10-28 06:39:37 +0900

    Fixed step_limit to 800_000
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_minigrid_memory_off_policy_pathwise.sh
+++ train_minigrid_memory_off_policy_pathwise.sh
@@ -7,7 +7,7 @@ python3 train.py pathwise \
   --agent_type off_policy \
   --encoder temporal_only \
   --temporal_model_type transformer \
-  --step_limit 400_000 \
+  --step_limit 800_000 \
   --use_done=1 \
   --denoising_time 0.8 \
   --disable_state_predictor=0 \
