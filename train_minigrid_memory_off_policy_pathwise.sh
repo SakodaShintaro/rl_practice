c7548d28625ea257e0917720537a5ea3d32ac662
sakoda <sakoda@keio.jp>
2025-11-03 22:19:12 +0900

    Removed temporal_model_type from scripts
    Signed-off-by: sakoda <sakoda@keio.jp>

--- train_minigrid_memory_off_policy_pathwise.sh
+++ train_minigrid_memory_off_policy_pathwise.sh
@@ -7,7 +7,6 @@ cd $(dirname $0)
 python3 train.py pathwise$suffix \
   --agent_type off_policy \
   --encoder temporal_only \
-  --temporal_model_type transformer \
   --step_limit 800_000 \
   --denoising_time 0.8 \
   --image_processor_type=simple_cnn \
--- train_minigrid_memory_on_policy_score.sh
+++ train_minigrid_memory_on_policy_score.sh
@@ -8,6 +8,5 @@ python3 train.py score$suffix \
   --agent_type on_policy \
   --use_action_value=0 \
   --encoder temporal_only \
-  --temporal_model_type gru \
   --disable_state_predictor=1 \
   --image_processor_type=simple_cnn \
