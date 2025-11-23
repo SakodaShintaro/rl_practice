e7da383612a770cba710754b2aa64d0a66982ddf
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-11-23 14:10:46 +0900

    Fixed action_text
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- networks/vlm.py
+++ networks/vlm.py
@@ -196,8 +196,7 @@ class QwenVLEncoder(nn.Module):
         x = hidden[:, -1, :].to(torch.float32)
 
         action_text = ""
-        # if messages:
-        #     action_text = self._generate_action_text(messages[0])
+        # action_text = self._generate_action_text(messages[0])
 
         return x, rnn_state, action_text
 
