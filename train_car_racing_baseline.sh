ed9da3b4ec79f81fd737b07b7596aa6ac75d0ce9
Shintaro Sakoda <sakoda@keio.jp>
2025-12-17 06:06:58 +0900

    Fixed scripts
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ train_car_racing_baseline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eux
+
+suffix=${1:-""}
+cd $(dirname $0)
+
+python3 train.py baseline$suffix \
+  --env_id CarRacing-v3 \
+  --agent_type off_policy \
+  --action_norm_penalty 1.0 \
+  --target_score 800.0 \
+  --encoder spatial_temporal \
+  --num_bins 1 \
+  --value_range 200.0 \
+  --step_limit 40_000 \
+  --eval_range 20 \
+  --image_processor_type ae \
+  --seq_len 8 \
+  --batch_size 32 \
+  --learning_rate 1e-4 \
