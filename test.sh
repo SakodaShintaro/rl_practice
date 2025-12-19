cec5a1edc61e23d1b9854bf45a6874c6074c0456
Shintaro Sakoda <sakoda@keio.jp>
2025-12-20 06:00:35 +0900

    Added source
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
+source .venv/bin/activate
+
 python3 train.py test --debug --agent_type off_policy --encoder spatial_temporal --temporal_model_type transformer
 python3 train.py test --debug --agent_type off_policy --encoder temporal_only
 python3 train.py test --debug --agent_type on_policy --encoder temporal_only
