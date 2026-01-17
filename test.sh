9ee10d7e93bdaa7d46e73645dace06045114fa41
Shintaro Sakoda <sakoda@keio.jp>
2026-01-17 14:20:25 +0900

    Fixed test.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ set -eux
 
 source .venv/bin/activate
 
-python3 train.py test --debug --agent_type off_policy --encoder spatial_temporal
-python3 train.py test --debug --agent_type off_policy --encoder temporal_only
-python3 train.py test --debug --agent_type off_policy --network_class actor_critic_with_action_value
-python3 train.py test --debug --agent_type on_policy --encoder temporal_only
+python3 train.py test --debug --batch_size 2 --agent_type off_policy --network_class actor_critic_with_action_value
+python3 train.py test --debug --batch_size 2 --agent_type off_policy --network_class actor_critic_with_action_value --encoder qwenvl
+python3 train.py test --debug --batch_size 2 --agent_type on_policy --network_class actor_critic_with_state_value
