534615bdb08ac691c789839d0276640fe823d785
Shintaro Sakoda <sakoda@keio.jp>
2026-01-17 15:47:49 +0900

    Fixed train_car_racing_on_policy.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_car_racing_on_policy.sh
+++ train_car_racing_on_policy.sh
@@ -7,15 +7,17 @@ cd $(dirname $0)
 python3 train.py baseline$suffix \
   --env_id CarRacing-v3 \
   --agent_type on_policy \
+  --network_class actor_critic_with_state_value \
   --action_norm_penalty 1.0 \
   --target_score 800.0 \
   --encoder spatial_temporal \
   --num_bins 1 \
-  --value_range 200.0 \
-  --step_limit 40_000 \
+  --value_range 60.0 \
+  --step_limit 200_000 \
   --eval_range 20 \
   --image_processor_type ae \
   --seq_len 8 \
   --batch_size 32 \
   --learning_rate 1e-4 \
+  --buffer_capacity 2048 \
   --policy_type Beta \
