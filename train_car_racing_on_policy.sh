77a34125212fe1decc5db0cb60a7ee16b104e936
Shintaro Sakoda <sakoda@keio.jp>
2026-01-21 08:11:20 +0900

    Fixed ppo
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_car_racing_on_policy.sh
+++ train_car_racing_on_policy.sh
@@ -10,14 +10,19 @@ python3 train.py baseline$suffix \
   --network_class actor_critic_with_state_value \
   --action_norm_penalty 1.0 \
   --target_score 800.0 \
-  --encoder spatial_temporal \
+  --encoder temporal_only \
   --num_bins 1 \
   --value_range 60.0 \
   --step_limit 200_000 \
   --eval_range 20 \
-  --image_processor_type ae \
-  --seq_len 8 \
+  --image_processor_type simple_cnn \
+  --seq_len 4 \
   --batch_size 32 \
   --learning_rate 1e-4 \
   --buffer_capacity 2048 \
   --policy_type beta \
+  --on_policy_epoch 10 \
+  --disable_state_predictor 1 \
+  --detach_actor 0 \
+  --critic_loss_weight 2.0 \
+  --max_grad_norm 0.5 \
