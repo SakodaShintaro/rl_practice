e70c05c27e105534b324f8aaf0a980d0e9e91404
Shintaro Sakoda <sakoda@keio.jp>
2026-01-25 19:23:19 +0900

    Fixed test.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ set -eux
 
 uv run python train.py test --debug --batch_size 2 --agent_type on_policy --network_class actor_critic_with_state_value --policy_type beta
 uv run python train.py test --debug --batch_size 2 --agent_type off_policy --network_class actor_critic_with_action_value
-uv run python train.py test --debug --batch_size 2 --agent_type on_policy --network_class vlm_actor_critic_with_state_value
+# uv run python train.py test --debug --batch_size 2 --agent_type on_policy --network_class vlm_actor_critic_with_state_value --seq_len 1
