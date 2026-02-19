632576d0cc738a9334ccad8f9d7f3ef03f3d6c8a
Shintaro Sakoda <sakoda@keio.jp>
2026-02-19 16:20:34 +0900

    Added train_color_panel.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ train_color_panel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eux
+
+suffix=${1:-""}
+cd $(dirname $0)
+
+# ゲームウィンドウが立ち上がっていなければ自動起動
+if ! wmctrl -l | grep -qi "Color Panel Game"; then
+  uv run python src/rl_practice/envs/color_panel_game.py &
+  sleep 2
+fi
+
+uv run python scripts/train.py baseline$suffix \
+  --env_id ColorPanel-v0 \
+  --agent_type off_policy \
+  --action_norm_penalty 0.0 \
+  --encoder spatial_temporal \
+  --num_bins 1 \
+  --value_range 200.0 \
+  --step_limit 40_000 \
+  --image_processor_type ae \
+  --seq_len 1 \
+  --batch_size 64 \
+  --learning_rate 1e-5 \
+  --render 1 \
+  --use_done 0 \
+  --gamma 0.0 \
+  --disable_state_predictor 1 \
