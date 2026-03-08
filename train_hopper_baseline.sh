09340aebff7ec582801a5dddc3e882b4d46343a2
sakoda <sakoda@keio.jp>
2026-03-08 23:53:39 +0900

    Fixed step_limit
    Signed-off-by: sakoda <sakoda@keio.jp>

--- train_hopper_baseline.sh
+++ train_hopper_baseline.sh
@@ -8,6 +8,6 @@ uv run python scripts/train.py baseline$suffix \
   --env_id Hopper-v5 \
   --agent_type off_policy \
   --network_class actor_critic_with_action_value \
-  --step_limit 500_000 \
+  --step_limit 100_000 \
   --batch_size 16 \
   --learning_rate 1e-4 \
