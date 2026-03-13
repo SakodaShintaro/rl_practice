6f616075f773ec6bf77f902f805e049a3daefd0c
Shintaro Sakoda <sakoda@keio.jp>
2026-03-13 16:48:19 +0900

    Fixed the settings of `train_color_panel.sh`
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_color_panel.sh
+++ train_color_panel.sh
@@ -7,10 +7,9 @@ cd $(dirname $0)
 uv run python scripts/train.py baseline$suffix \
   --env_id ColorPanel-v0 \
   --agent_type off_policy \
+  --network_class vlm_actor_critic_with_action_value \
   --step_limit 40_000 \
-  --batch_size 64 \
+  --batch_size 16 \
   --learning_rate 1e-5 \
-  --render 1 \
   --use_done 0 \
-  --gamma 0.0 \
   --disable_state_predictor 1 \
