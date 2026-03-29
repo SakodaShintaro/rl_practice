179dfa0fe8e59888a55ca2bff789b2cbd0f4ffc0
Shintaro Sakoda <sakoda@keio.jp>
2026-03-30 06:45:59 +0900

    Added train_all_parallel.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ train_all_parallel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+set -eux
+
+RESULT_DIR=${1:?"Usage: $0 <result_dir>"}
+RESULT_DIR=$(readlink -f "$RESULT_DIR")
+
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+
+pids=()
+for gpu_id in $(seq 0 $((NUM_GPUS - 1))); do
+  CUDA_VISIBLE_DEVICES=$gpu_id bash train_all.sh "${RESULT_DIR}/${gpu_id}" "_gpu${gpu_id}" &
+  pids+=($!)
+done
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
