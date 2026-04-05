524ae81ab9d7da0122a8bc42e591e5c33a8e6a57
Shintaro Sakoda <sakoda@keio.jp>
2026-04-05 10:12:37 +0900

    Fixed the carla script
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_carla_off_policy.sh
+++ train_carla_off_policy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -eux
 
 trap 'kill 0' EXIT
 
+suffix=${1:-""}
+cd $(dirname $0)
+
 pgrep -f CarlaUE4 > /dev/null || ~/CARLA_0.9.16/CarlaUE4.sh -RenderOffScreen &
 
-uv run python scripts/train.py carla_test \
+uv run python scripts/train.py carla$suffix \
     --env_id CARLA-Leaderboard-v0 \
     --agent_type off_policy \
     --step_limit 400_000 \
-    --disable_state_predictor 1 \
+    --network_class vlm_actor_critic_with_action_value \
