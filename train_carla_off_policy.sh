fa1455d242550b1bd254b08362277d254e4e9634
Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>
2026-04-08 08:12:50 +0900

    Added learning_rate
    Signed-off-by: Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>

--- train_carla_off_policy.sh
+++ train_carla_off_policy.sh
@@ -12,4 +12,5 @@ uv run python scripts/train.py carla$suffix \
     --env_id CARLA-Leaderboard-v0 \
     --agent_type off_policy \
     --step_limit 400_000 \
+    --learning_rate 1e-5 \
     --network_class vlm_actor_critic_with_action_value \
