744e5f03330ee61815c53e7cfdc685229e2ed8ac
Shintaro Sakoda <sakoda@keio.jp>
2026-04-16 13:44:11 +0900

    Added highlight=False
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- src/vla_streaming_rl/wrappers.py
+++ src/vla_streaming_rl/wrappers.py
@@ -57,7 +57,7 @@ def _car_racing_parse_action(action_text: str) -> tuple[np.ndarray, bool]:
 
 def make_env(env_id: str) -> gym.Env:
     if env_id == "BabyAI-GoToLocal-v0":
-        env = gym.make(env_id, render_mode="rgb_array")
+        env = gym.make(env_id, render_mode="rgb_array", highlight=False)
         env = minigrid.wrappers.RGBImgObsWrapper(env, tile_size=32)
         env = minigrid.wrappers.ImgObsWrapper(env)
         env = ReduceActionSpaceWrapper(env, n_actions=3)
