dfa8abbdbfc7a090682c6685bac037cf76f9869b
Shintaro Sakoda <sakoda@keio.jp>
2026-04-21 22:05:52 +0900

    Fixed train_car_racing*
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- train_car_racing_baseline.sh
+++ train_car_racing_baseline.sh
@@ -4,7 +4,12 @@ set -eux
 suffix=${1:-""}
 cd $(dirname $0)
 
+AGENT=cnn_off_policy_bs16
+# AGENT=base_on_policy
+# AGENT=vlm_off_policy_bs16
+# AGENT=vlm_streaming
+
 uv run python scripts/train.py \
-  agent=cnn_off_policy_bs16 \
+  agent=${AGENT} \
   env=car_racing \
-  exp_name=baseline$suffix
+  exp_name=${AGENT}${suffix}
